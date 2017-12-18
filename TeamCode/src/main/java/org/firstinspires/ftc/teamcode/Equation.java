e059d5cde9def911656fc8831f7e46a1a68787fd
ablauch <32077598+ablauch@users.noreply.github.com>
2017-12-18 13:17:38 -0500

    save it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Equation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Equation.java
@@ -26,7 +26,7 @@ public class Equation extends LinearOpMode {
 
         robot.init(hardwareMap);
 
-        double Vx; fred
+        double Vx;
         double Vy;
         Math W = atan (Vy / Vx);
         double O;

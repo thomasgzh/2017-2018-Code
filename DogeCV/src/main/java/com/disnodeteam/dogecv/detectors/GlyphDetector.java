019f2ecbc37bd628877955ed8006fd4e704f67af
wiindly <32077149+wiindly@users.noreply.github.com>
2018-01-05 14:41:05 -0500

    testing testing 123

--- DogeCV/src/main/java/com/disnodeteam/dogecv/detectors/GlyphDetector.java
+++ DogeCV/src/main/java/com/disnodeteam/dogecv/detectors/GlyphDetector.java
@@ -1,10 +1,9 @@
 package com.disnodeteam.dogecv.detectors;
 
-import android.media.Image;
-
 import com.disnodeteam.dogecv.OpenCVPipeline;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
@@ -25,52 +24,125 @@ import java.util.List;
 
 public class GlyphDetector extends OpenCVPipeline {
 
-    public double ChosenGlyphPos = -1;
-    public double ChosenGlyphOffset = 0;
 
-    public boolean FoundRect = false;
-
-    public Size ImageSize = new Size(480, 360);
-
-    public boolean DebugShowPreprocessed = false;
-    public boolean DebugShowFiltered     = false;
-    public boolean DebugDrawStats        = false;
-    public boolean DebugDrawCenter       = false;
-    public boolean DebugDrawRects        = true;
-
-    public double ScoreRatioWeight = 0.5;
-    public double ScoreDistanceXWeight = 0.8;
-    public double ScoreDistanceYWeight = 1;
-    public double ScoreAreaWeight = 0.4;
-
-    public double MinScore = 0.5;
-
-    public Mat MatOverride = new Mat();
-
-
-    public void SetTestMat( int rId){
-        try {
-            Mat imported = Utils.loadResource(context, rId);
-            Imgproc.cvtColor(imported,imported,Imgproc.COLOR_RGB2BGR);
-            Imgproc.resize(imported,MatOverride, new Size(1280,960));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //hardwareMap.appContext, com.qualcomm.ftcrobotcontroller.R.drawable.test_cv, CvType.CV_8UC4);
+    public enum GlyphDetectionMode {
+        EDGE
     }
 
+    public enum GlyphDetectionSpeed {
+        VERY_FAST, FAST, BALANCED, SLOW, VERY_SLOW
+    }
+
+    //Settings
+
+    public GlyphDetectionMode  detectionMode        = GlyphDetectionMode.EDGE;
+    public double              downScaleFactor      = 0.6;
+    public boolean             rotateMat            = false;
+    public double              minScore             = 0.5;
+    public double              scoreRatioWeight     = 0.5;
+    public double              scoreDistanceXWeight = 0.8;
+    public double              scoreDistanceYWeight = 1.1;
+    public double              scoreAreaWeight       = 0.5;
+    public GlyphDetectionSpeed speed                = GlyphDetectionSpeed.BALANCED;
+    public boolean             debugDrawStats       = false;
+    public boolean             debugDrawRects       = true;
+
+
+    //results
+    private Point chosenGlyphPosition = null;
+    private double chosenGlyphOffset = 0;
+    private boolean foundRect = false;
+
+    private Mat workingMat = new Mat();
+    private Mat edges = new Mat();
+    private Mat processed = new Mat();
+    private Mat structure = new Mat();
+    private Size newSize  = new Size();
     @Override
     public Mat processFrame(Mat rgba, Mat gray) {
-        Mat out = rgba.clone();
-        Imgproc.resize(out,out, ImageSize);
 
-        Mat processed = preProcessFrame(out);
-        Mat mask = filterFrame(processed);
+        Size initSize = rgba.size();
+        newSize = new Size(initSize.width * downScaleFactor, initSize.height * downScaleFactor);
+
+        rgba.copyTo(workingMat);
+        Imgproc.resize(workingMat,workingMat, newSize);
+
+        if(rotateMat){
+            Mat tempBefore = workingMat.t();
+            Core.flip(tempBefore, workingMat, 1); //mRgba.t() is the transpose
+            tempBefore.release();
+        }
+
+
+        Imgproc.putText(workingMat,newSize.toString() + " - " + speed.toString(),new Point(5,15),0,0.5,new Scalar(0,255,0),1);
+
+        Imgproc.cvtColor(workingMat,processed,Imgproc.COLOR_RGB2GRAY);
+
+        switch (speed){
+            case VERY_FAST:
+
+                Imgproc.blur(processed,processed,new Size(2,2));
+
+                Imgproc.bilateralFilter(processed.clone(),processed,11,17,17);
+
+                Imgproc.Canny(processed,edges,15,45.0);
+
+                structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(3,3));
+                Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
+                break;
+            case FAST:
+                Imgproc.blur(processed,processed,new Size(3,3));
+
+                Imgproc.bilateralFilter(processed.clone(),processed,11,17,17);
+
+                Imgproc.Canny(processed,edges,15,45.0);
+
+                structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(6,6));
+                Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
+                break;
+
+            case BALANCED:
+                Imgproc.blur(processed,processed,new Size(4,4));
+
+                Imgproc.bilateralFilter(processed.clone(),processed,11,17,17);
+
+                Imgproc.Canny(processed,edges,15,45.0);
+
+                structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(7,7));
+                Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
+                break;
+
+
+            case SLOW:
+
+                Imgproc.blur(processed,processed,new Size(6,6));
+
+                Imgproc.bilateralFilter(processed.clone(),processed,11,17,17);
+
+                Imgproc.Canny(processed,edges,15,45.0);
+
+
+                structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(10,10));
+                Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
+                break;
+
+            case VERY_SLOW:
+
+                Imgproc.blur(processed,processed,new Size(7,7));
+
+                Imgproc.bilateralFilter(processed.clone(),processed,11,17,17);
+
+                Imgproc.Canny(processed,edges,15,45.0);
+
+                structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(15,15));
+                Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
+                break;
+        }
 
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
 
-        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
         hierarchy.release();
 
         double chosenScore = 0;
@@ -88,9 +160,7 @@ public class GlyphDetector extends OpenCVPipeline {
                 }
             }
         });
-        if(contours.size() <= 0){
-            return rgba;
-        }
+
         contours.remove(0); // Remove First Index which is usually a large square filling the entire screen,
 
         for(MatOfPoint c : contours) {
@@ -104,26 +174,25 @@ public class GlyphDetector extends OpenCVPipeline {
                 double h = rect.height;
 
                 Point centerPoint = new Point(x + ( w/2), y + (h/2));
-
                 double cubeRatio = Math.max(Math.abs(h/w), Math.abs(w/h));
 
                 double score = 100;
 
                 double diffrenceFromPerfect = Math.abs(1 - cubeRatio);
                 double scoreRatioPunishment = 1 - diffrenceFromPerfect;
-                double scoreRatio = scoreRatioPunishment * ScoreRatioWeight;
+                double scoreRatio = scoreRatioPunishment * scoreRatioWeight;
                 score *= scoreRatio;
 
-                double distanceFromCenterX = (ImageSize.width / 2) - centerPoint.x;
-                double distanceFromCenterY = ImageSize.height - centerPoint.y;
-                distanceFromCenterX = Math.abs(distanceFromCenterX / ImageSize.width);
-                distanceFromCenterY = Math.abs(distanceFromCenterY / ImageSize.height);
+                double distanceFromCenterX = (newSize.width / 2) - centerPoint.x;
+                double distanceFromCenterY = newSize.height - centerPoint.y;
+                distanceFromCenterX = Math.abs(distanceFromCenterX / newSize.width);
+                distanceFromCenterY = Math.abs(distanceFromCenterY / newSize.height);
 
                 double scoreDistanceFromCenterXPunishment = 1 - distanceFromCenterX;
                 double scoreDistanceFromCenterYPunishment = 1 - distanceFromCenterY;
 
-                double scoreDistanceFromCenterX = scoreDistanceFromCenterXPunishment * ScoreDistanceXWeight;
-                double scoreDistanceFromCenterY = scoreDistanceFromCenterYPunishment * ScoreDistanceYWeight;
+                double scoreDistanceFromCenterX = scoreDistanceFromCenterXPunishment * scoreDistanceXWeight;
+                double scoreDistanceFromCenterY = scoreDistanceFromCenterYPunishment * scoreDistanceYWeight;
 
                 score *= scoreDistanceFromCenterX;
                 score *= scoreDistanceFromCenterY;
@@ -133,7 +202,7 @@ public class GlyphDetector extends OpenCVPipeline {
                 double area = Imgproc.contourArea(c);
                 double normalizedArea = (area - minArea) / (maxArea - minArea);
                 double scoreAreaPunishment = normalizedArea;
-                double scoreArea  =scoreAreaPunishment * ScoreAreaWeight;
+                double scoreArea  =scoreAreaPunishment * scoreAreaWeight;
                 score *= scoreArea;
 
                 if(chosenRect == null){
@@ -146,12 +215,13 @@ public class GlyphDetector extends OpenCVPipeline {
                     chosenScore = score;
                 }
 
-                if(DebugDrawRects){
-                    Imgproc.rectangle(out,new Point(x,y), new Point((x+w),(y+h)),new Scalar(0,255,255),1);
+                if(debugDrawRects){
+                    Imgproc.rectangle(workingMat,new Point(x,y), new Point((x+w),(y+h)),new Scalar(0,255,255),1);
                 }
 
-                if(DebugDrawStats){
-                    Imgproc.putText(out,"Score: " + score, new Point(x+5,y+5),0,0.5, new Scalar(0,255,255));
+                if(debugDrawRects){
+                    String toPrint = String.format("Score: %.2f",score);
+                    Imgproc.putText(workingMat,toPrint , new Point(x+5,y+5),0,0.5, new Scalar(0,255,255));
                 }
             }
 
@@ -159,49 +229,27 @@ public class GlyphDetector extends OpenCVPipeline {
 
 
         }
-        if(chosenRect != null && chosenScore > MinScore){
+        if(chosenRect != null && chosenScore > minScore){
             double x = chosenRect.x;
             double y = chosenRect.y;
             double w = chosenRect.width;
             double h = chosenRect.height;
-            Imgproc.rectangle(out,new Point(x,y), new Point((x+w),(y+h)),new Scalar(0,255,0),3);
+            Imgproc.rectangle(workingMat,new Point(x,y), new Point((x+w),(y+h)),new Scalar(0,255,0),3);
 
 
-            ChosenGlyphPos = (x+(w/2));
-            ChosenGlyphOffset = ImageSize.width - (x+(w/2)) ;
+            chosenGlyphPosition = new Point((x+(w/2)), (y+(h/2)));
+            chosenGlyphOffset = newSize.width - (x+(w/2)) ;
 
-            FoundRect = false;
+            foundRect = false;
         }else{
-            FoundRect = true;
+            foundRect = true;
         }
-        Imgproc.resize(out,out,new Size(1280,960));
 
-        return out;
+        Imgproc.resize(workingMat,workingMat,initSize);
+
+        return workingMat;
     }
 
-
-    public Mat preProcessFrame(Mat inputFrame){
-        Mat processed = inputFrame.clone();
-        Imgproc.resize(processed,processed, ImageSize);
-        Imgproc.cvtColor(processed,processed,Imgproc.COLOR_RGBA2GRAY);
-        return processed;
-    }
-
-    public Mat filterFrame(Mat inputFrame){
-        Mat blurred = new Mat();
-        Imgproc.blur(inputFrame,blurred,new Size(5,5));
-
-        Mat blat = blurred.clone();
-        Imgproc.bilateralFilter(blurred,blat,11,17,17);
-
-        Mat edges = new Mat();
-        Imgproc.Canny(blurred,edges,20,50.0);
-
-        Mat structure = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(10,10));
-        Imgproc.morphologyEx(edges,edges,Imgproc.MORPH_CLOSE,structure);
-
-        return edges;
-    }
 
     private double GetMaxArea(List<MatOfPoint> allConturs){
         double currentMax = 0;
@@ -227,5 +275,21 @@ public class GlyphDetector extends OpenCVPipeline {
         }
 
         return currentMax;
+    }
+
+    public Point getChosenGlyphPosition() {
+        return chosenGlyphPosition;
+    }
+
+    public double getChosenGlyphOffset() {
+        return chosenGlyphOffset;
+    }
+
+    public boolean isFoundRect() {
+        return foundRect;
+    }
+
+    public Size getFrameSize() {
+        return newSize;
     }
 }
